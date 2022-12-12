030b45ec74aeea69cd67b590781cbe649c784afe
gab-che <gabriellachessa1@gmail.com>
2022-12-12 15:54:31 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+    import TheHeader from './components/TheHeader.vue';
+    import TheMain from './components/TheMain.vue';
+    export default{
+        components: { TheHeader, TheMain }
+
+    }
+
+</script>
+
+<template>
+    <TheHeader />
+    <TheMain />
+</template>
+
+<style lang="scss">
+    @use './styles/general.scss';
+    @use './styles/partials/variables' as *;
+    @use './styles/partials/utilities' as *;
+</style>
---
+++ src/components/TheMain.vue
@@ -0,0 +1,13 @@
+<script>
+    export default{}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+    @use '../styles/general.scss';
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/utilities' as *;
+</style>
