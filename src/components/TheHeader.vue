15063aa7fcf373adafd33df7637eede846579be3
gab-che <gabriellachessa1@gmail.com>
2022-12-12 18:52:06 +0100

    prints simple movie list (on search)

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -11,7 +11,7 @@
 
         methods: {
             searchTitle(title){
-                this.store. searchInput = title;
+                this.store.searchInput = title;
                 fetchMovies();
             }
         }
@@ -21,6 +21,14 @@
 <template>
     <h1>BoolFlix</h1>
     <TheSearchSection @search-title="searchTitle"/>
+    <ul>
+        <li v-for="movie in store.movies">
+            <h5>Titolo: {{movie.title}}</h5>
+            <p>Titolo originale: {{movie.original_title}}</p>
+            <p>Lingua: {{movie.original_language}}</p>
+            <p>Voto: {{movie.vote_average}}</p>
+        </li>
+    </ul>
 </template>
 
 <style lang="scss" scoped>
