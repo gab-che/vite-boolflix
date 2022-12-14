2b9598081f9bc0b617192cf57fe4ec633f1b04b7
gab-che <gabriellachessa1@gmail.com>
2022-12-14 22:14:28 +0100

    adds style to header

--- src/components/TheSearchSection.vue
+++ src/components/TheSearchSection.vue
@@ -5,6 +5,7 @@
                 userInput: {
                     title: '',
                 },
+                isHidden: false,
             }
         },
 
@@ -13,21 +14,31 @@
         methods: {
             onSearchClick(){
                 this.$emit('search-title', this.userInput.title)
-            }
+            },
+            
         },
+
+
     }
 </script>
 
 <template>
     <div class="search_bar">
-        <div class="input-group">
-            <input type="text" class="form-control d-none" placeholder="Cerca film o serie tv"
-                v-model="userInput.title"
-                @keyup.enter="onSearchClick">
-            <button class="btn" type="button">
-                <i class="fa-solid fa-magnifying-glass"></i>
-            </button>
-        </div>
+        <ul class="icon_list">
+            <li>
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Cerca film o serie tv"
+                        v-model="userInput.title"
+                        v-if="isHidden"
+                        @keyup.enter="onSearchClick">
+                    <button class="btn" type="button" @click="isHidden = !isHidden">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </div>
+            </li>
+            <li><a href="#"><i class="fa-solid fa-bell"></i></a></li>
+            <li><a href="#"><i class="fa-solid fa-user"></i></a></li>
+        </ul>
     </div>
 
 </template>
@@ -37,7 +48,11 @@
     @use '../styles/partials/variables' as *;
     @use '../styles/partials/utilities' as *;
 
-    i{
-        color: $primary_color;
+    .icon_list{
+        @include flex_gap;
+        
+        i{
+            color: $primary_color;
+        }
     }
 </style>
